d9560f46dbf635aa9af2d457ee76644e96a3ac08
basseuph@gmail.com <basseuph@gmail.com>
2021-09-21 19:29:33 -0700

    Security info now secure including DEBUG, ALLOWED_HOSTS, and SECURITY_KEY.

--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -11,22 +11,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yar%!=15@66f!88gf30=+x4h%fb(bh#g*@m(t=%mkt*09n#kof'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 # Application definition
 
